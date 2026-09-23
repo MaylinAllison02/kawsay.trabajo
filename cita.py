77bb889f0b54ce6f14e13775c569271a53b80d3f
MaylinAllison02 <allisonmaylinchavez@gmail.com>
2026-09-22 23:47:38 -0500

    Update patient registration details in cita.py

---
+++ cita.py
@@ -0,0 +1,14 @@
+registar paciente
+registar medico 
+buscar por codigo programar por cita
+registar atencion
+buscar medico por especialidad
+consultar historial
+salir
+seleccione una opcion: 4
+registar paciente 
+codigo del paciente: p001
+el codigo del medico: m003
+fecha de la cita 24/09/26
+codigo de la cita: c006
+cita programada correctamente
