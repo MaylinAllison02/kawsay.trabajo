aea800456ed57257fe52d853b7559553b2e301a7
MaylinAllison02 <allisonmaylinchavez@gmail.com>
2026-09-22 23:44:30 -0500

    Implement patient and doctor registration features
    Added patient registration and medical functionalities.

---
+++ medico.py
@@ -0,0 +1,13 @@
+registrar paciente
+registar medico 
+buscar codigo
+programar cita
+registrar atencion
+buscar medico por especialidad
+consultar historial
+salir
+seleccione una opcion: 1 
+codigo del paciente:p005
+nombre del paciente: rocio mamani 
+edad: cuarenta 
+la edad debe ser el numero 
