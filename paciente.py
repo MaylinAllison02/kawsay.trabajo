89b6902dc1e10d7d1f7b75197653cf99a23e5b8d
MaylinAllison02 <allisonmaylinchavez@gmail.com>
2026-09-22 23:41:01 -0500

    Add patient registration and consultation options

---
+++ paciente.py
@@ -0,0 +1,11 @@
+registar paciente
+registar medico 
+buscar por codigo programar por cita
+registar atencion
+buscar medico por especialidad
+consultar historial
+salir
+seleccione una opcion: 1
+registar paciente 
+codigo del paciente: p001
+el codigo ya esta registrado
