a80e5b4e12c190cc707316c2a5c4fc715bf0fdf9
MaylinAllison02 <allisonmaylinchavez@gmail.com>
2026-09-22 23:50:40 -0500

    Add patient registration and validation prompts

---
+++ main.py
@@ -0,0 +1,13 @@
+registar paciente
+registar medico 
+buscar por codigo programar por cita
+registar atencion
+buscar medico por especialidad
+consultar historial
+salir
+seleccione una opcion: 1
+registar paciente 
+codigo del paciente: p005
+nombre del paciente: andres juarez 
+edad: 0
+la edad debe estar entre 1 y 119
